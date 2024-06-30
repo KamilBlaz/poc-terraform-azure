02bb1c3924664b9258dc2fd240330e478fb8a188
Kamil Błaż <blazkamil@outlook.com>
2024-06-30 14:53:49 +0200

    POC; work in progresS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.110.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
 
 resource "azurerm_resource_group" "this" {
-    name     = "test"
-    location = "West Europe"
+  name     = "test"
+  location = "West Europe"
 }
